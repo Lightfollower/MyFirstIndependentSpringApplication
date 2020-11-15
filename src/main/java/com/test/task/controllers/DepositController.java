bb143351d2d259731a8f9cf3c966c6e7858d644a
Doom <lightfollower1985@gmail.com>
2020-11-15 20:41:06 +0500

    DepositController, put, обработка ошибок и исправлена NPE при неполных данных о клиенте и банке.

--- src/main/java/com/test/task/controllers/DepositController.java
+++ src/main/java/com/test/task/controllers/DepositController.java
@@ -5,9 +5,11 @@ import com.test.task.entities.Client;
 import com.test.task.entities.Deposit;
 import com.test.task.entities.dtos.DepositDto;
 import com.test.task.exceptions.MalformedEntityException;
+import com.test.task.exceptions.NullIdException;
 import com.test.task.services.BankService;
 import com.test.task.services.ClientService;
 import com.test.task.services.DepositService;
+import com.test.task.utils.Constants;
 import com.test.task.utils.DepositFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -37,7 +39,7 @@ public class DepositController {
     }
 
     @GetMapping
-    public List<DepositDto> getDeposits(@RequestParam Map<String, String> requestParams){
+    public List<DepositDto> getDeposits(@RequestParam Map<String, String> requestParams) {
         int pageNumber = Integer.parseInt(requestParams.getOrDefault(PAGE_STRING, "0"));
         Client client = null;
 //        Список вкладов по имени клиента
@@ -54,14 +56,14 @@ public class DepositController {
     }
 
 
-//    Достаточно указать id клиента и банка в теле запроса
+    //    Достаточно указать id клиента и банка в теле запроса
     @PostMapping
-    public ResponseEntity<?> add(@RequestBody @Validated Deposit deposit, BindingResult bindingResult){
+    public ResponseEntity<?> add(@RequestBody @Validated Deposit deposit, BindingResult bindingResult) {
         if (deposit.getId() != null) {
             deposit.setId(null);
         }
         if (bindingResult.hasErrors())
-            throw new MalformedEntityException();
+            throw new MalformedEntityException(Constants.allFieldsMustBeFilled);
 //        Нужно для того, чтобы указывать только id банка и клиента.
         deposit.setClient(clientService.getClientById(deposit.getClient().getId()));
         deposit.setBank(bankService.getBankById(deposit.getBank().getId()));
@@ -70,15 +72,21 @@ public class DepositController {
     }
 
     @PutMapping
-    public ResponseEntity<?> modify(@RequestBody @Validated Deposit deposit, BindingResult bindingResult){
+    public ResponseEntity<?> modify(@RequestBody @Validated Deposit deposit, BindingResult bindingResult) {
         if (deposit.getId() == null) {
-            throw new RuntimeException("Deposit id can't be null");
+            throw new NullIdException();
         }
-        return new ResponseEntity<>(depositService.saveOrUpdate(deposit),HttpStatus.OK);
+        if (bindingResult.hasErrors())
+            throw new MalformedEntityException(Constants.allFieldsMustBeFilled);
+//        Нужно для того, чтобы указывать только id банка и клиента.
+        deposit.setClient(clientService.getClientById(deposit.getClient().getId()));
+        deposit.setBank(bankService.getBankById(deposit.getBank().getId()));
+        
+        return new ResponseEntity<>(depositService.saveOrUpdate(deposit), HttpStatus.OK);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<?> delete(@PathVariable Long id){
+    public ResponseEntity<?> delete(@PathVariable Long id) {
         depositService.deleteById(id);
         return new ResponseEntity<>(HttpStatus.OK);
     }
