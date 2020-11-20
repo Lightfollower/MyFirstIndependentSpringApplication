3d19c65a3ccbd414ed9d15af44d4e8f7cbede341
Doom <lightfollower1985@gmail.com>
2020-11-20 17:02:21 +0500

    Обработка ошибки кривого JSON, с фронтенда.

--- src/main/java/com/test/task/exceptions/ExceptionControllerAdvice.java
+++ src/main/java/com/test/task/exceptions/ExceptionControllerAdvice.java
@@ -2,6 +2,7 @@ package com.test.task.exceptions;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -25,5 +26,10 @@ public class ExceptionControllerAdvice {
     @ExceptionHandler
     public ResponseEntity<?> handleNullIdException(NullIdException exc) {
         return new ResponseEntity<>(exc.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<?> handleMalformedJSONFromFrontEnd(HttpMessageNotReadableException exc) {
+        return new ResponseEntity<>("Ошибка в формировании JSON объекта", HttpStatus.I_AM_A_TEAPOT);
     }
 }
