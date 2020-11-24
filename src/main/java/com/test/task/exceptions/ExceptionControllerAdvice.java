f2f04b92271abb238b503715fc8bcddd1c5eedb8
Doom <lightfollower1985@gmail.com>
2020-11-24 22:00:01 +0500

    ExceptionControllerAdvice. handleResourceNotFoundException

--- src/main/java/com/test/task/exceptions/ExceptionControllerAdvice.java
+++ src/main/java/com/test/task/exceptions/ExceptionControllerAdvice.java
@@ -1,16 +1,18 @@
 package com.test.task.exceptions;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
 @Slf4j
 @ControllerAdvice
 public class ExceptionControllerAdvice {
     @ExceptionHandler
-    public ResponseEntity<?> handleIdException(nonExistentIdException exc) {
+    public ResponseEntity<?> handleIdException(NonExistentIdException exc) {
         log.error(exc.getMessage(), exc);
         return new ResponseEntity<>(exc.getMessage(), HttpStatus.NOT_FOUND);
     }
@@ -37,5 +39,11 @@ public class ExceptionControllerAdvice {
     public ResponseEntity<?> handleMalformedJSONFromFrontEnd(HttpMessageNotReadableException exc) {
         log.error(exc.getMessage(), exc);
         return new ResponseEntity<>("Ошибка в формировании JSON объекта", HttpStatus.I_AM_A_TEAPOT);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<?> handleResourceNotFoundException(ResourceNotFoundException exc) {
+        log.error(exc.getMessage(), exc);
+        return new ResponseEntity<>(exc.getMessage(), HttpStatus.NOT_FOUND);
     }
 }
