49f87e57c6d580f1bafdc3ee12fdc9584df3c3f9
Doom <lightfollower1985@gmail.com>
2020-11-24 21:58:36 +0500

    FormController, log.

--- src/main/java/com/test/task/controllers/FormController.java
+++ src/main/java/com/test/task/controllers/FormController.java
@@ -4,11 +4,12 @@ import com.test.task.entities.Form;
 import com.test.task.entities.dtos.FormDto;
 import com.test.task.exceptions.MalformedEntityException;
 import com.test.task.exceptions.NullIdException;
-import com.test.task.exceptions.nonExistentIdException;
+import com.test.task.exceptions.NonExistentIdException;
 import com.test.task.services.FormService;
 import com.test.task.utils.Constants;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,6 +22,7 @@ import java.util.List;
 @RequestMapping("api/v1/forms")
 @RestController
 @Api("Set of endpoints for CRUD operations for forms")
+@Slf4j
 public class FormController {
     private FormService formService;
 
@@ -32,12 +34,14 @@ public class FormController {
     @GetMapping
     @ApiOperation("Returns list of all banks")
     public List<FormDto> get(){
+        log.info("get forms");
         return formService.findAll();
     }
 
     @PostMapping
     @ApiOperation("Creates a new form. If id != null, then it will be cleared")
     public ResponseEntity<?> addForm(@RequestBody @Validated Form form, BindingResult bindingResult){
+        log.info("Saving new form");
         if(form.getId() == null)
             form.setId(null);
         if (bindingResult.hasErrors())
@@ -48,6 +52,7 @@ public class FormController {
     @PutMapping
     @ApiOperation("Modifies an existing form")
     public ResponseEntity<?> updateForm(@RequestBody @Validated Form form, BindingResult bindingResult){
+        log.info("Update form");
         if(form.getId() == null)
             throw new NullIdException();
         if (bindingResult.hasErrors())
@@ -58,8 +63,7 @@ public class FormController {
     @DeleteMapping("/{id}")
     @ApiOperation("Deletes a form from the system. 404 if the form's identifier is not found.")
     public void delete(@PathVariable Long id){
-        if (!formService.existsById(id))
-            throw new nonExistentIdException(Constants.noObjectWithThisId);
+        log.info("delete form with id: " + id);
         formService.deleteById(id);
     }
 }
