8b1a814f86963b1d1228d353e7c18d5e973af4e3
Doom <lightfollower1985@gmail.com>
2020-11-24 22:09:49 +0500

    FormRepository, existByName().

--- src/main/java/com/test/task/repositories/FormRepository.java
+++ src/main/java/com/test/task/repositories/FormRepository.java
@@ -7,4 +7,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface FormRepository extends JpaRepository<Form, Long>{
     Form getByName(String name);
+    Boolean existsByName(String name);
 }
