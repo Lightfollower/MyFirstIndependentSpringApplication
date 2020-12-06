7378e60416f58246df7fe6b18dfddbd9ccd278c3
Doom <lightfollower1985@gmail.com>
2020-12-07 01:26:30 +0500

    RegistrationController, Logger и Swagger

--- src/main/java/com/test/task/controllers/RegistrationController.java
+++ src/main/java/com/test/task/controllers/RegistrationController.java
@@ -6,6 +6,9 @@ import com.test.task.exceptions.NonExistentIdException;
 import com.test.task.exceptions.NullIdException;
 import com.test.task.services.UserService;
 import com.test.task.utils.Constants;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.http.HttpStatus;
@@ -15,8 +18,12 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/v1/register")
+@Api("Set of endpoints for CRUD operations for users")
+@Slf4j
 public class RegistrationController {
     private UserService userService;
 
@@ -31,8 +38,17 @@ public class RegistrationController {
         dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
     }
 
+    @GetMapping
+    @ApiOperation("Returns list of all users with empty password fields")
+    public List<SystemUser> getUsers(){
+        log.info("Get users");
+        return userService.findAll();
+    }
+
     @PostMapping
+    @ApiOperation("Creates a new user with role admin. If id != null, then it will be cleared")
     public ResponseEntity<?> registerNewAdministrator(@RequestBody @Validated SystemUser systemUser, BindingResult bindingResult) {
+        log.info("Saving new user");
         if (systemUser.getName() != null)
             systemUser.setId(null);
         if (bindingResult.hasFieldErrors("name"))
@@ -44,9 +60,11 @@ public class RegistrationController {
     }
 
     @PutMapping
+    @ApiOperation("Modifies an existing user")
     public ResponseEntity<?> updateAdministrator(@RequestBody @Validated SystemUser systemUser, BindingResult bindingResult) {
         if (systemUser.getId() == null)
             throw new NullIdException();
+        log.info("Updating user with id " + systemUser.getId());
         if (systemUser.getName() == null)
             throw new MalformedEntityException("Field name must be filled");
         if (!userService.existById(systemUser.getId()))
@@ -58,8 +76,9 @@ public class RegistrationController {
     }
 
     @DeleteMapping("/{id}")
+    @ApiOperation("Deletes a user from the system. 404 if the users's identifier is not found.")
     public void deleteClientById(@PathVariable Long id) {
-//        log.info("Deleting client with id " + id);
+        log.info("Deleting user with id " + id);
         userService.deleteById(id);
     }
 }
